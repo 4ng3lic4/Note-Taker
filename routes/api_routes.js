457eab91e70b084b437b6ad2d18a2aec9c314d35
4ng3lic4 <ositosviajeros@hotmail.com>
2022-12-14 08:35:02 -0500

    add fs writeFile and JSON

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -13,10 +13,17 @@ router.get('/notes', (req,res)=> {
 
 router.post('/notes', (req,res)=>{
     //This is how we add  the id to the note
+    console.log(req.body);
     req.body.id= uuidv4()
     //push the req body into the array of notes in the db
+    db.push(req.body);
     //after tat, the db variabele will have the new note
     //we need to rerite the json file with the new note using fs
+    fs.writeFile('./db/db.json', JSON.stringify(db), (err) => {
+        if (err) throw err 
+        console.log('this file has been saved')
+    } );
+    res.json(req.body);
     //refer to the README assignment for that
 })
 
